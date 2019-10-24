a542bd388e2e40b05cd036171d5c6665d0560052
lorix <foxred324@gmail.com>
2019-10-25 00:49:30 +0300

    java script lessons

---
+++ conv_script.js
@@ -0,0 +1,5 @@
+alert("There are converter!")
+var pounds = prompt("Write there the wheit in pounds (lbs)")
+var kg = pounds*0.454
+alert("That is:  "+kg+" kilograms")
+console.log("Conversion Completed")
