f07dc9827793fc02a9fbc5fd7b7e948f2286b01d
lorix <foxred324@gmail.com>
2019-12-06 01:37:04 +0200

    jQuerry events

---
+++ jQuerry.md
@@ -0,0 +1,3 @@
+###ССылки на ресурс
+
+https://api.jquery.com/category/events/
--- myscript.js
+++ myscript.js
@@ -1,3 +1,22 @@
-$('h1').click(function() {
-  $(this).text('I was changed!')
+// $('h1').click(function() {
+//   $(this).text('I was changed!')
+// })
+
+$('input').eq(0).keypress(function (event) {
+  if (event.which === 13) {
+    $('h3').toggleClass('turnBlue');
+  }
+})
+
+
+$('h1').on('mouseenter', function() {
+  $(this).toggleClass('turnRed');
+})
+
+// $('input').eq(1).on('click',function() {
+//   $('.container').fadeOut(3000);
+// })
+
+$('input').eq(1).on('dblclick',function() {
+  $('.container').slideUp(3000);
 })
