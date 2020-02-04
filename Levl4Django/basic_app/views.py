a1938ea264e2315fdd36279824e7cbdb4dd98fcc
lorix <foxred324@gmail.com>
2020-02-04 19:29:44 +0200

    create form vithout validation

--- Levl4Django/basic_app/views.py
+++ Levl4Django/basic_app/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from .import forms
-
+from django.views.decorators.csrf import csrf_exempt
 # Create your views here.
 
 
@@ -9,7 +9,16 @@ def index(request):
     return render(request, 'basic_app/index.html')
 
 
+@csrf_exempt
 def register(request):
     """Registr form."""
     form = forms.FormName()
+    if request.method == 'POST':
+        form = forms.FormName(request.POST)
+        if form.is_valid():
+            print("validation success")
+            print("Nme " + form.cleaned_data['name'])
+            print("Email " + form.cleaned_data['email'])
+            print("Text " + form.cleaned_data['text'])
+
     return render(request, 'basic_app/forms_page.html', {'form': form})
